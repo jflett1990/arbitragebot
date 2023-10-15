96a1d7e2051c023aa71b56fabf4856fb006488a0
jflett1990 <81728344+jflett1990@users.noreply.github.com>
2023-10-14 20:11:44 -0400

    Create libraries.js

---
+++ libraries.js
@@ -0,0 +1,23 @@
+// Import the necessary libraries (e.g., Web3.js)
+
+// Connect to the Ethereum network
+const web3 = new Web3(Web3.givenProvider);
+
+// Add any additional libraries required for your project
+const axios = require('axios');
+const moment = require('moment');
+const lodash = require('lodash');
+const fs = require('fs');
+const path = require('path');
+// Add more libraries as needed
+
+// Export the required libraries
+module.exports = {
+  web3,
+  axios,
+  moment,
+  lodash,
+  fs,
+  path,
+  // Export additional libraries here
+};
